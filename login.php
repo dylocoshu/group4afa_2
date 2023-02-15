48b564e1300ea6949fce7bf1452d8c8a61eafe88
dylocoshu <117916361+dylocoshu@users.noreply.github.com>
2023-02-15 23:24:11 +0000

    Add files via upload

--- login.php
+++ login.php
@@ -1,15 +1,12 @@
 <?php
-
 $db = new SQLite3('/xampp/Data/EverybodyWelcomeDB.db');
 $DBH = new PDO("sqlite:/xampp/Data/EverybodyWelcomeDB.db");
 
 if(isset($_POST['submit'])){
 
-   
-
-   
      //Retrieve the user account information for the given username.
      $username = $_POST['username'];
+     $password = $_POST['pass'];
      $sql = "SELECT * FROM Business_Owner WHERE Username = :username";
      $stmt = $DBH->prepare($sql);
      
@@ -22,9 +19,18 @@ if(isset($_POST['submit'])){
      //Fetch row.
      $user = $stmt->fetch(PDO::FETCH_ASSOC);
      
+     
      //If $row is FALSE.
      if($user === false){
         echo '<script>alert("invalid username or password")</script>';
+     }
+     elseif($user['Password'] != $password){
+      echo '<script>alert("invalid username or password")</script>';
+      
+     }
+     else{
+      echo '<script>alert("Logged in !")</script>';
+      header("Location: homepage.php");
      }
   
    
@@ -34,21 +40,18 @@ if(isset($_POST['submit'])){
 
 }
 
-
 ?>
-
-<!DOCTYPE html>
 <html lang="en">
+<?php require("NavBar.php"); ?>
+
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
    <title>Login</title>
-
-  
-
 </head>
-<body>
+<body>   
 
 <section class="form-container">
 
@@ -56,7 +59,7 @@ if(isset($_POST['submit'])){
       <h3>welcome back!</h3>
       <input type="username" name="username" required maxlength="50" placeholder="enter your username" class="box">
       <input type="password" name="pass" required maxlength="20" placeholder="enter your password" class="box">
-      <p>don't have an account? <a href="register.html">register new</a></p>
+      <p>don't have an account? <a href="register.php">register new</a></p>
       <input type="submit" value="login now" name="submit" class="btn">
    </form>
 
