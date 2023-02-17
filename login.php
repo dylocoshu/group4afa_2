ca062472667d564ecf47649a1be878a22abc314f
Edward Foster <alienblaster50@gmail.com>
2023-02-17 10:51:23 +0000

    init

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if(isset($_POST['submit'])){
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="login_style.css">
-   <title>Login</title>
+   <title>Login </title>
 </head>
 <body>   
 
