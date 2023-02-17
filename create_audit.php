bfd042bdbfd3f75de33068ef4dfba42b1ac6bc47
Edward Foster <alienblaster50@gmail.com>
2023-02-17 14:53:41 +0000

    init

--- create_audit.php
+++ create_audit.php
@@ -1,7 +1,7 @@
 
 
 <?php 
-
+$amount = 0;
 $db = new SQLite3('/xampp/Data/test.db');
 
 
@@ -14,6 +14,34 @@ if(isset($_POST['add-question'])){
     $stmt->bindParam(":AP", $_POST['action-point']);
     $result = $stmt->execute();
 }
+
+if (isset($_POST['venue-type'])){
+    $venue=$_POST['venue-type'];
+    $sql = "SELECT Question, Action_Point, QuestionID FROM Questions WHERE Venue_Type = :venue";
+    $stmt = $db -> prepare($sql);
+    $stmt->bindParam(":venue", $venue);
+    $result = $stmt->execute();
+    $rows_array = [];
+    $amount = 0;
+
+
+    while ($row=$result->fetchArray())
+    {
+    $amount += 1;
+    $rows_array[]=$row;
+    }
+}
+
+if(isset($_POST['delete'])){
+    echo $_POST['delete'];
+    $sql = "DELETE from Questions WHERE QuestionID = :id";
+    $stmt = $db -> prepare($sql);
+    $stmt->bindParam(":id", $_POST['delete']);
+    $result = $stmt->execute();
+}
+
+
+      
 ?>
 
 <html>
@@ -35,7 +63,8 @@ if(isset($_POST['add-question'])){
                 <tr>
                     <td> <label > Select a Venue </label></td>
                     <td><input type = "search" name ="venue-type" placeholder="Search"> </td>
-                    <td><button type = "button" name="venue-button"> Venue Questions </button> </td>
+                    <td><button type = "submit" name="venue-button"> Venue Questions </button> </td>
+                    
                 </tr>
                 <tr>
                     <td> <label > Question: </label></td>
@@ -52,4 +81,25 @@ if(isset($_POST['add-question'])){
         </div>
     </div>
 </form>
+<form method="POST">
+                <div class = "homepage-table">
+                    <table style="width: 100%; text-align: center;">
+                        <tr class="tableHead">
+                            <th>Question</th>
+                            <th>Action Point</th>
+                            <th></th>
+                            
+                        </tr>
+                        <?php for($x = 0  ; $x < $amount; $x+=1){;?>
+                                    <tr>
+                                        <td><strong><?php echo $rows_array[$x][0]?></strong></td>
+                                        <td><strong><?php echo $rows_array[$x][1];?></strong></td>
+                                        <?php if($rows_array[$x][1]){ ?><td> <button type="submit" name="delete" value= <?php echo $rows_array[$x][2] ?>> Delete </button></td><?php } ?>
+                                      
+                                    </tr>
+                                    <?php }?>
+                        </table>
+                                </div>
+                        </form>
+                               
 </html>
