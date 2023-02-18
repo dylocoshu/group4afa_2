2cf14aded2a42e7e2407606c00edaaf018aa03b2
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-02-18 00:03:09 +0000

    init

---
+++ About_temp.php
@@ -0,0 +1,108 @@
+
+<html>
+  <?php require("verify_login.php");?>
+<head>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="about_style.css">
+<title>Everybody Welcome</title>
+	
+	<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+p {
+  margin-bottom: 1.5em;
+}
+
+h2, h3, h4 {
+  margin-top: 1.5em;
+}
+
+@media (max-width: 480px) {
+ 
+  h2 {
+    font-size: 20px;
+  }
+  h3 {
+    font-size: 18px;
+  }
+}
+
+
+
+
+
+
+
+main {
+  padding: 20px;
+  margin: 20px;
+  background-color: #F8F8F8;
+  border-radius: 5px;
+  border: 2px solid #4169E1;
+}
+
+a {
+  color: #4169E1;
+  text-decoration: none;
+  transition: text-decoration 0.3s;
+}
+a:hover {
+  text-decoration: underline;
+}
+
+footer {
+  background-color: #F2F2F2;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+
+
+	</style>
+</head>
+<body>
+
+<main>
+  <div class = "page-grid">
+    <div class = "ps1-header"> <h1> About Us </h1> </div>
+</br>
+  <section>
+    <div class = "page-section-1"> 
+      <div class = "about-us-image">
+        <img src = 'images/museum_1.png'  alt="Everybody Welcome" width = 612 height = 408>
+      </div>
+      <div class = "about-us-text" style = "border-image: round;">
+      <p>Everybody Welcome!There are one billion people in the world with additional access needs, including over 14 million disabled people in the U.K. 93% will search for access information about your venue before they visit and, if they can’t find what they are looking for, 
+        41% will take their business elsewhere meaning that you could be missing out on valuable custom just for the sake of providing relevant information*.
+        But what information should you provide? How should you provide it, when should you promote it and where?Everybody Welcome is the easy way to share your accessibility information and welcome the world.
+        So what are you waiting for? </p>
+      </div>
+    </div>
+  </section>
+  </div>
+</main>
+
+
+
+
+
+<footer>
+    <p>&copy; Everybody Welcome</p>
+</footer>
+</html>
