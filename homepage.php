13550a9607024a8dc56ca046d9dc2f16e8ea107f
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-03-07 14:51:47 +0000

    filler

--- homepage.php
+++ homepage.php
@@ -46,7 +46,7 @@ while ($row=$result->fetchArray())
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="homepage_style.css">
-        <title>Everybodly Welcome</title>
+        <title>Everybodlly Welcome</title>
     </head>
 <main>
     <div class = "homepage-grid">
