02cc0724ceae0eba174e90077a5d2031e3b07c3e
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-03-14 15:42:04 +0000

    From MySQL to PDO

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=a4a-server-group4.mysql.database.azure.com;dbname=a4adatabase';
+$user = 'izqhdzlktw';
+$password = '315Y87ZNG4I53V17$';
+try { 
+$db = new PDO($dsn, $user, $password); 
+$db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$db ->exec("SET CHARACTER SET utf8");
+}
+catch (PDOException $e) { 
+echo 'Connection failed again: ' . $e->getMessage();
+}
+?>
