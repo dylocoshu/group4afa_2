ba4ee8527289e447ee183d6043c7b64d8a793cc1
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-03-15 14:43:24 +0000

    .

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ $dsn = 'mysql:host=a4a-server-group4.mysql.database.azure.com;dbname=a4a-databas
 $user = 'izqhdzlktw';
 $password = '315Y87ZNG4I53V17$';
 try { 
-#$db = new PDO($dsn, $user, $password); 
+$db = new PDO($dsn, $user, $password); 
 $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 $db ->exec("SET CHARACTER SET utf8");
 }
