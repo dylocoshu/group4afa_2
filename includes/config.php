3eb8a99dc065c9012caa7e2cac1e5382919ebca3
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-03-27 13:36:46 +0100

    BF

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,12 @@
 <?php
 #sd
-// $dsn = 'mysql:host=localhost;dbname=a4adatabase';
-// $user = 'root';
-// $password = '';
+$dsn = 'mysql:host=localhost;dbname=a4adatabase';
+$user = 'root';
+$password = '';
 
-$dsn = 'mysql:host=afagroup4.mysql.database.azure.com;dbname=afadatabase';
-$user = 'admingroup4';
-$password = 'Passwordgroup41!';
+// $dsn = 'mysql:host=afagroup4.mysql.database.azure.com;dbname=afadatabase';
+// $user = 'admingroup4';
+// $password = 'Passwordgroup41!';
 try { 
 $db = new PDO($dsn, $user, $password); 
 $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
