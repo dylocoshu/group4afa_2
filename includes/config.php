23ec2ac5750ce4fc8cde13354095d8a42f39947a
Dylan OConnor <c0014965@hallam.shu.ac.uk>
2023-03-27 11:05:57 +0100

    azure config

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 #sd
-// $dsn = 'mysql:host=a4a-server-group4.mysql.database.azure.com;dbname=a4a-database';
-// $user = 'izqhdzlktw';
-// $password = '315Y87ZNG4I53V17$';
+// $dsn = 'mysql:host=localhost;dbname=a4adatabase';
+// $user = 'root';
+// $password = '';
 
 $dsn = 'mysql:host=afagroup4.mysql.database.azure.com;dbname=afadatabase';
 $user = 'admingroup4';
@@ -11,7 +11,6 @@ try {
 $db = new PDO($dsn, $user, $password); 
 $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 $db ->exec("SET CHARACTER SET utf8");
-echo "connected";
 }
 catch (PDOException $e) { 
 echo 'Connection failed again: ' . $e->getMessage();
