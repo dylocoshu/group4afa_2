268e4b5ac3ae2391eee38ff891f2e460fbab4ec4
Edward Foster <alienblaster50@gmail.com>
2023-03-28 10:54:53 +0100

    Tidying

--- view_access_features.php
+++ view_access_features.php
@@ -63,8 +63,8 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles/style.css">
-        <link rel="stylesheet" href="styles/homepage_style.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="homepage_style.css">
         <title>Everybody Welcome</title>
     </head>
 
