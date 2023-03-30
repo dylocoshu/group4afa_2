d747ab07bfd4c33b1afa2acbb272d522ce0c6566
Edward Foster <alienblaster50@gmail.com>
2023-03-30 09:38:02 +0100

    added user side

---
+++ about1.php
@@ -0,0 +1,340 @@
+<!DOCTYPE HTML>
+
+<html>
+<?php require("nav2.php"); ?>
+	<head>
+		<title>About Us</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main1.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="left-sidebar is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+							<img src="images/logo.png" alt="" />
+							</header>
+						</div>
+
+					<!-- Nav -->
+						
+
+				</div>
+
+			<!-- Main -->
+				<div class="wrapper style1">
+
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-4 col-12-mobile" id="sidebar">
+								<hr class="first" />
+								<section>
+									<header>
+										<h3><a href="#">Are you a business?</a></h3>
+									</header>
+									<p>
+										By signing up you can make your business visable to those who need it.
+									</p>
+									<footer>
+										<a href="business-info.php" class="button">Learn More</a>
+									</footer>
+								</section>
+								<hr />
+								<section>
+									<header>
+										<h3><a href="#">Sed lorem etiam consequat</a></h3>
+									</header>
+									<p>
+										Tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+									</p>
+									<div class="row gtr-50">
+										<div class="col-4">
+											<a href="#" class="image fit"><img src="images/pic10.jpg" alt="" /></a>
+										</div>
+										<div class="col-8">
+											<h4>Nibh sed cubilia</h4>
+											<p>
+												Amet nullam fringilla nibh nulla convallis tique ante proin.
+											</p>
+										</div>
+										<div class="col-4">
+											<a href="#" class="image fit"><img src="images/pic11.jpg" alt="" /></a>
+										</div>
+										<div class="col-8">
+											<h4>Proin sed adipiscing</h4>
+											<p>
+												Amet nullam fringilla nibh nulla convallis tique ante proin.
+											</p>
+										</div>
+										<div class="col-4">
+											<a href="#" class="image fit"><img src="images/pic12.jpg" alt="" /></a>
+										</div>
+										<div class="col-8">
+											<h4>Lorem feugiat magna</h4>
+											<p>
+												Amet nullam fringilla nibh nulla convallis tique ante proin.
+											</p>
+										</div>
+										<div class="col-4">
+											<a href="#" class="image fit"><img src="images/pic13.jpg" alt="" /></a>
+										</div>
+										<div class="col-8">
+											<h4>Sed tempus fringilla</h4>
+											<p>
+												Amet nullam fringilla nibh nulla convallis tique ante proin.
+											</p>
+										</div>
+										<div class="col-4">
+											<a href="#" class="image fit"><img src="images/pic14.jpg" alt="" /></a>
+										</div>
+										<div class="col-8">
+											<h4>Malesuada fermentum</h4>
+											<p>
+												Amet nullam fringilla nibh nulla convallis tique ante proin.
+											</p>
+										</div>
+									</div>
+									<footer>
+										<a href="#" class="button">Magna Adipiscing</a>
+									</footer>
+								</section>
+							</div>
+							<div class="col-8 col-12-mobile imp-mobile" id="content">
+								<article id="main">
+									<header>
+										<h2><a href="#">About us</a></h2>
+										<p>
+											This is our story....
+										</p>
+									</header>
+									<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+									<p>There are one billion people in the world with additional access needs, including over 14 million disabled people in the U.K.
+									<br><br>93% will search for access information about your venue before they visit and, if they can’t find what they are looking for, 41% will take their business elsewhere meaning that you could be missing out on valuable custom just for the sake of providing relevant information.
+									<br><br>But what information should you provide? How should you provide it, when should you promote it and where?
+									<br><br>Everybody Welcome is the easy way to share your accessibility information and welcome the world.
+									<br><br>So what are you waiting for?
+									</p>
+									<section>
+										<header>
+											<h3>What is Everybody Welcome?</h3>
+										</header>
+										<p>
+										Everybody Welcome is a straightforward, smart tool to help you record, share and market relevant accessibility information about your venue.
+										</p>
+										<p>
+										
+										</p>
+									</section>
+									<section>
+										<header>
+											<h3>Why is it needed?</h3>
+										</header>
+										<p>
+										Research shows that more than half of disabled people are not confident about visiting new places and that 79% have had a disappointing trip or had to change their plans at the last minute due to poor accessibility. 77% also find that accessibility information hard to find or inaccurate. (*Source: Euan’s Guide Access Survey 2019)
+										Venues, on the other hand, are unsure what information provide or find existing methods of providing access information costly, complicated or time-consuming so often provide nothing.
+
+As a result, people with additional access needs are missing out on visits and experiences and businesses are missing out on the additional income that would be generated by those visits.
+
+Everybody Welcome is designed to change that by supporting businesses to provide relevant accessibility information easily and quickly and enabling consumers to plan visits with ease.
+										</p>
+									</section>
+								</article>
+							</div>
+						</div>
+						<hr />
+						<div class="row">
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Gravida aliquam penatibus</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Sed quis rhoncus placerat</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+							<article class="col-4 col-12-mobile special">
+								<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+								<header>
+									<h3><a href="#">Magna laoreet et aliquam</a></h3>
+								</header>
+								<p>
+									Amet nullam fringilla nibh nulla convallis tique ante proin sociis accumsan lobortis. Auctor etiam
+									porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat integer interdum.
+								</p>
+							</article>
+						</div>
+					</div>
+
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
+
+							<!-- Tweets -->
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon brands fa-twitter circled"><span class="label">Tweets</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="tweet">
+												Amet nullam fringilla nibh nulla convallis tique ante sociis accumsan.
+												<span class="timestamp">5 minutes ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Hendrerit rutrum quisque.
+												<span class="timestamp">30 minutes ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Curabitur donec nulla massa laoreet nibh. Lorem praesent montes.
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="tweet">
+												Lacus natoque cras rhoncus curae dignissim ultricies. Convallis orci aliquet.
+												<span class="timestamp">5 hours ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
+
+							<!-- Posts -->
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon solid fa-file circled"><span class="label">Posts</span></h2>
+									</header>
+									<ul class="divided">
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Nisl fermentum integer</a></h3>
+												</header>
+												<span class="timestamp">3 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Phasellus portitor lorem</a></h3>
+												</header>
+												<span class="timestamp">6 hours ago</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Magna tempus consequat</a></h3>
+												</header>
+												<span class="timestamp">Yesterday</span>
+											</article>
+										</li>
+										<li>
+											<article class="post stub">
+												<header>
+													<h3><a href="#">Feugiat lorem ipsum</a></h3>
+												</header>
+												<span class="timestamp">2 days ago</span>
+											</article>
+										</li>
+									</ul>
+								</section>
+
+							<!-- Photos -->
+								<section class="col-4 col-12-mobile">
+									<header>
+										<h2 class="icon solid fa-camera circled"><span class="label">Photos</span></h2>
+									</header>
+									<div class="row gtr-25">
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic10.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic11.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic12.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic13.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic14.jpg" alt="" /></a>
+										</div>
+										<div class="col-6">
+											<a href="#" class="image fit"><img src="images/pic15.jpg" alt="" /></a>
+										</div>
+									</div>
+								</section>
+
+						</div>
+						<hr />
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Contact -->
+									<section class="contact">
+										<header>
+											<h3>Nisl turpis nascetur interdum?</h3>
+										</header>
+										<p>Urna nisl non quis interdum mus ornare ridiculus egestas ridiculus lobortis vivamus tempor aliquet.</p>
+										<ul class="icons">
+											<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+											<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
+											<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+											<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">Linkedin</span></a></li>
+										</ul>
+									</section>
+
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ search.php
@@ -0,0 +1,141 @@
+<?php require("verify_login.php");?>
+
+<!-- query for search bar>
+<?php
+    $query = $_GET['query'];
+    
+    if($query){
+        $location = "";
+        $searchValue = $query;
+        $features_array= [];
+        if(!empty($_POST['features'])) {    
+            foreach($_POST['features'] as $value){
+                array_push($features_array, $value);
+            }
+        }
+        $features_string= implode(",",$features_array);
+    }
+    $sql_stmnt = "SELECT Business_Name, Venue_Type, Business_Description, Access_Features, Location, Postcode, BusinessID FROM Business_Owner 
+    WHERE Business_Name IS NOT NULL";
+    if(!empty($location)){
+        $sql_stmnt.= " AND Location = '$location'";
+    }
+    if(!empty($searchValue)){
+        $sql_stmnt.= " AND LOWER(Business_Description) LIKE LOWER('%$searchValue%') ";
+    }
+    if(!empty($features_string)){
+        $sql_stmnt.= " AND Access_Features LIKE '$features_string'";
+    }
+    $stmt = $db->prepare($sql_stmnt);
+    
+     $stmt->execute();
+    
+    
+
+
+
+    ?>
+
+
+<!DOCTYPE html>
+
+<?php
+#$db = new SQLite3('/xampp/Data/test.db');
+
+
+if(isset($_POST['submit'])){
+    $location = $_POST['locationDropdown'];
+    $searchValue = $_POST['searchValue'];
+    $features_array= [];
+    if(!empty($_POST['features'])) {    
+        foreach($_POST['features'] as $value){
+            array_push($features_array, $value);
+        }
+    }
+    $features_string= implode(",",$features_array);
+}
+$sql_stmnt = "SELECT Business_Name, Venue_Type, Business_Description, Access_Features, Location, Postcode, BusinessID FROM Business_Owner 
+WHERE Business_Name IS NOT NULL";
+if(!empty($location)){
+    $sql_stmnt.= " AND Location = '$location'";
+}
+if(!empty($searchValue)){
+    $sql_stmnt.= " AND LOWER(Business_Description) LIKE LOWER('%$searchValue%') ";
+}
+if(!empty($features_string)){
+    $sql_stmnt.= " AND Access_Features LIKE '$features_string'";
+}
+$stmt = $db->prepare($sql_stmnt);
+
+ $stmt->execute();
+
+#'
+
+
+
+?>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="homepage_style.css">
+        <title>Everybody Welcome</title>
+    </head>
+<main>
+    <div class = "homepage-grid">
+        <div class = "homepage-search">
+        <form method = "POST">
+        <body>  
+            <b>Location :  </b>
+            <select name="locationDropdown" id="locationDropdown">
+                <option value="">Any</option>  
+                <option value="Sheffield" <?php if(!empty($location)){if(strpos($location, "Sheffield") !== FALSE){?> selected = "true" <?php }}?>>Sheffield</option>
+                <option value="Derby" <?php if(!empty($location)){if(strpos($location, "Derby") !== FALSE){?> selected = "true" <?php }}?>>Derby</option>  
+                <option value="London" <?php if(!empty($location)){if(strpos($location, "London") !== FALSE){?> selected = "true" <?php }}?>>London</option>  
+            </select>
+            <input type="search" name="searchValue" placeholder="Search" value=<?php if(isset($_POST['submit'])){;}?>>
+            <button class="w-20 btn btn-primary" style="align: center; float:left;" type="submit" name="submit" value="submitLocation">Search</button>  
+
+        </body>
+    </div>
+        <body>
+            <?php //if ($amount != 0){?>
+                <div class = "homepage-table">
+                    <table style="width: 100%; text-align: center;">
+                        <tr class="tableHead">
+                            <th>Business Name</th>
+                            <th>Venue</th>
+                            <th>Access Features</th>
+                            <th>Location</th>
+                            <th>Postcode</th>
+                        </tr>
+                        <?php while ($row=$stmt->fetchObject()){?>
+                                    <tr>
+                                        <td><a href="https://www.cineworld.co.uk/cinemas/sheffield/031"><strong><?php echo $row->Business_Name?></strong></a></td>
+                                        <td><strong><?php echo $row->Venue_Type;?></strong></td>
+                                        <td><strong><a href="view_access_features.php?id=<?php echo $row->BusinessID;?>">View Access Features</a></strong></td>
+                                        <td><strong><?php echo $row->Location;?></strong></td>
+                                        <td><strong><?php echo $row->Postcode;?></strong></td>
+                                    </tr>
+                                    <?php }?>
+                        </table>
+                                </div>
+                                <?php //}?>
+        </body>
+        <div class = "homepage-access-features">
+            <header><b> Filter by Access Features </b></header>
+                <input type="checkbox" id="vehicle1" name='features[]' value="Ramp" <?php if(!empty($features_string)){if(strpos($features_string, "Ramp") !== FALSE){?> checked <?php }}?> >
+                <label for="vehicle1"> Ramp</label><br>
+                <input type="checkbox" id="vehicle2" name='features[]' value="Accessible Toilets" <?php if(!empty($features_string)){if(strpos($features_string, "Accessible Toilets") !== FALSE) {?> checked <?php }}?>>
+                <label for="vehicle2"> Accessible Toilets</label><br>
+                <input type="checkbox" id="vehicle3" name='features[]' value="Disabled Parking" <?php if(!empty($features_string)){if(strpos($features_string, "Disabled Parking") !== FALSE) {?> checked <?php }}?>>
+                <label for="vehicle3"> Disabled Parking</label><br>
+        </div>
+                                </form>
+    </div>
+
+                        </main>
+                        <?php require("Footer.php");?>
+                        </html>
+
