b8f1dc2125fcc2560acc3d7b064a2bc74ce17914
Edward Foster <alienblaster50@gmail.com>
2023-03-31 09:54:09 +0100

    navbar2

---
+++ NavBar2.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <style>
+      nav{
+        box-shadow: 0 2px 2px -2px rgba(0,0,0,5);
+        height: 197px !important;
+      }
+      a{
+        transition: all .2s ease-in-out; 
+        
+      }
+      a:hover{   
+         background-color: rgb(241, 236, 236);
+         border-radius: 10px;
+      }
+      img {
+  margin-top: 10px;
+}
+.new{
+  background-color: black;
+}
+.link{
+  color:white !important
+}
+
+
+      
+    </style>
+    <title>Everybody Welcome</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+	
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+  
+  </head>
+
+<body class = "link" >
+	<nav class = "new">
+		<div class = "nav-text-effects"> <img src = 'images/logowhite.png'  alt="Everybody Welcome" width = 290 height = 130  >  </div>
+		<div class = "nav-text-effects"><a class = "link" href="home.php" > Home</a></div>
+	</nav>
+</body>
+
